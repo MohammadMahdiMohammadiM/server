4f39b99ab63f9cb2e499e0a98e4acc69c945e7cc
Artan Arian <mmohammadimolasarayi@gmail.com>
2022-04-30 18:05:21 +0430

    Add Production Mode!

---
+++ AWA/Pages/Errors/Error.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWA.Pages.Errors
+{
+    public class ErrorModel : Infrastructure.BasePageModel
+    {
+        public ErrorModel()
+        {
+
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
--- AWA/Program.cs
+++ AWA/Program.cs
@@ -1,15 +1,44 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+var WebApplicationsOptions =
+    new Microsoft.AspNetCore.Builder.WebApplicationOptions
+    {
+        //EnvironmentName =
+        //Microsoft.Extensions.Hosting.Environments.Development,
+
+        EnvironmentName =
+        Microsoft.Extensions.Hosting.Environments.Production,
+    };
+
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(options: WebApplicationsOptions);
 
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+var app =
+    builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Errors/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapRazorPages();
 
